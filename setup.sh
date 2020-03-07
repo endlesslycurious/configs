f2231299547a21708c6ed02d7e14952805b7afa4
endlesslycurious <61634041+endlesslycurious@users.noreply.github.com>
2020-03-07 13:10:51 -0800

    Add Mac/Linux setup script

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Use 'chmod u+x setup.sh' to make this script executable
+
+if [ "$(uname)" == "Darwin" ]; then
+    # Mac OS X setup 
+
+	echo "Setting .vimrc"
+	ln -F -s ./.vimrc ~/.vimrc	
+
+	echo "Setting .bash_profile"
+	ln -F -s ./.bash_profile ~/.bash_profile	
+	
+	echo "Setting Brewfile"
+	ln -F -s ./Brewfile ~/Brewfile	
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    # GNU/Linux setup
+	echo "Not written yet!"		
+fi
