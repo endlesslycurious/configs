dd13c1105a1f48b7b21adcd8e7036f8d9c8bacc3
endlesslycurious <dpbrown@ea.com>
2020-03-12 10:40:40 -0700

    Add VSCode settings and update PC setup, mac setup still todo

--- setup.bat
+++ setup.bat
@@ -12,6 +12,11 @@ set target="%APPDATA%\Sublime Text 3\Packages\User\Preferences.sublime-settings"
 if exist %target% del %target%
 mklink %target% %CD%\Sublime3.sublime-settings
 
+REM VS Code
+set target="%APPDATA%\Code\User\settings.json" 
+if exist %target% del %target%
+mklink %target% %CD%\VSCode.json
+
 REM Chocolatey
 set target=%HOMEDRIVE%%HOMEPATH%\choco.config 
 if exist %target% del %target%
