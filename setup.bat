989641ee2b31c6883380f893ea299ab8b57e1e4b
endlesslycurious <61634041+endlesslycurious@users.noreply.github.com>
2020-03-13 11:27:21 -0700

    Setup VScode on mac

--- setup.bat
+++ setup.bat
@@ -2,22 +2,22 @@ REM setup symbolic links to config files
 @ECHO OFF
 @SETLOCAL
 
-REM VIM
+REM Configuring VIM
 set target=%HOMEDRIVE%%HOMEPATH%\_vimrc 
 if exist %target% del %target%
 mklink %target% %CD%\.vimrc
 
-REM Sublime 3
+REM Configuring Sublime 3
 set target="%APPDATA%\Sublime Text 3\Packages\User\Preferences.sublime-settings" 
 if exist %target% del %target%
 mklink %target% %CD%\Sublime3.sublime-settings
 
-REM VS Code
+REM Configuring VS Code
 set target="%APPDATA%\Code\User\settings.json" 
 if exist %target% del %target%
 mklink %target% %CD%\VSCode.json
 
-REM Chocolatey
+REM Configuring Chocolatey
 set target=%HOMEDRIVE%%HOMEPATH%\choco.config 
 if exist %target% del %target%
 mklink %target% %CD%\Chocolatey.config
--- setup.sh
+++ setup.sh
@@ -5,16 +5,19 @@
 if [ "$(uname)" == "Darwin" ]; then
     # Mac OS X setup 
 
-	echo "Setting .vimrc"
+	echo "Configuring .vimrc"
 	ln -F -s $PWD/.vimrc ~/.vimrc	
 
-	echo "Setting Sublime Text 3"
+	echo "Configuring Sublime Text 3"
 	ln -F -s $PWD/Sublime3.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
 
-	echo "Setting .bash_profile"
+	echo "Configuring VS Code"
+	ln -F -s $PWD/VSCode.json ~/Library/Application\ Support/Code/User/settings.json
+
+	echo "Configuring .bash_profile"
 	ln -F -s $PWD/.bash_profile ~/.bash_profile	
 	
-	echo "Setting Brewfile"
+	echo "Configuring Brewfile"
 	ln -F -s $PWD/Brewfile ~/Brewfile	
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     # GNU/Linux setup
