38839c7b0db63004d65d9c90b659b48a0dcb3fcd
endlesslycurious <dpbrown@ea.com>
2020-10-15 18:36:30 -0700

    clean up temp choco files

--- update.bat
+++ update.bat
@@ -7,4 +7,7 @@ choco upgrade all -y
 echo --- Installing from manifest ---
 choco install Chocolatey.config -y
 
+echo --- Delete temp install files ---
+rmdir /Q /S C:\Users\dpbrown\AppData\Local\Temp\chocolatey
+
 echo --- Done! ---
