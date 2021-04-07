cf852e232029ece0c6cd0e637abe2406b83c528f
endlesslycurious <61634041+endlesslycurious@users.noreply.github.com>
2021-04-07 12:07:45 -0700

    Pin python to 3.8.9 as P4Python doesn't yet support 3.9. Add python module installation

--- update.bat
+++ update.bat
@@ -1,13 +1,20 @@
 echo --- List outdated packages ---
 choco outdated
 
-echo --- Upgrading outdated packages ---
-choco upgrade all -y
-
 echo --- Installing from manifest ---
 choco install Chocolatey.config -y
 
+echo --- Pinning Python versions ---
+choco pin add -n=python
+choco pin add -n=python3
+
+echo --- Upgrading outdated packages ---
+choco upgrade all -y
+
 echo --- Delete temp install files ---
 rmdir /Q /S C:\Users\dpbrown\AppData\Local\Temp\chocolatey
+
+echo --- Install Python modules ---
+pip install -r python-modules.txt
 
 echo --- Done! ---
