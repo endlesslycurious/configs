ae59f34f1e0f1f31d96c6f5930e9a1e216aad492
endlesslycurious <61634041+endlesslycurious@users.noreply.github.com>
2021-04-07 12:11:36 -0700

    Better echo descriptions

--- update.bat
+++ update.bat
@@ -1,20 +1,23 @@
-echo --- List outdated packages ---
+echo --- Choco: List outdated packages ---
 choco outdated
 
-echo --- Installing from manifest ---
+echo --- Choco: Installing from manifest ---
 choco install Chocolatey.config -y
 
-echo --- Pinning Python versions ---
+echo --- Choco: Pinning Python versions ---
 choco pin add -n=python
 choco pin add -n=python3
 
-echo --- Upgrading outdated packages ---
+echo --- Choco: Upgrading outdated packages ---
 choco upgrade all -y
 
-echo --- Delete temp install files ---
+echo --- Choco: Delete temp install files ---
 rmdir /Q /S C:\Users\dpbrown\AppData\Local\Temp\chocolatey
 
-echo --- Install Python modules ---
+echo --- Python: Update PIP ---
+python.exe -m pip install --upgrade pip
+
+echo --- Python: Install modules ---
 pip install -r python-modules.txt
 
 echo --- Done! ---
