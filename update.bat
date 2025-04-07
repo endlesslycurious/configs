53bd73ce1eca91c79c40623de6e42f711aa374c5
endlesslycurious <61634041+endlesslycurious@users.noreply.github.com>
2025-04-07 16:38:14 -0700

    Update batch file to reflecct file moves/tidying

--- update.bat
+++ update.bat
@@ -10,7 +10,12 @@ if ERRORLEVEL 1 (
 )
 
 REM Install apps from Microsoft Store via winget
-set store=winget/%COMPUTERNAME%-winget.txt
+set store=winget/%COMPUTERNAME%.txt
+
+if NOT EXIST %store% (
+	REM use default if no machine specific exists
+	set store=winget/default.txt
+)
 
 if EXIST %store% (
 	echo --- WinGet: Installing from manifest ---
@@ -24,7 +29,12 @@ if EXIST %store% (
 )
 
 REM Update pip if python installed
-set piplist=pip/%COMPUTERNAME%-pip.txt
+set piplist=pip/%COMPUTERNAME%.txt
+
+if NOT EXIST %piplist% (
+	REM use default if no machine specific exists
+	set piplist=pip/default.txt
+)
 
 find /C "Python" %store% > NUL
 if %ERRORLEVEL% EQU 0 (
@@ -43,6 +53,11 @@ if %ERRORLEVEL% EQU 0 (
 REM Install VS.Code extensions listed in code-ext.txt, generated 
 REM by running 'code --list-extensions >> code-ext.txt'
 set exts=vscode/%COMPUTERNAME%-code-ext.txt
+
+if NOT EXIST %exts% (
+	REM use default if no machine specific exists
+	set exts=vscode/code-ext.txt
+)
 
 if EXIST %exts% (
 	echo --- VS Code: Installing Extensions ---
