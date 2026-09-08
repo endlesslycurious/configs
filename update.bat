aeb87c14a6df25b15c5b2ff674330f2f9b781daf
endlesslycurious <61634041+endlesslycurious@users.noreply.github.com>
2026-09-08 12:29:38 -0700

    Remove pip step, was just deploy pipenv on windows. Now using UV.

--- update.bat
+++ update.bat
@@ -22,32 +22,11 @@ if EXIST %store% (
 	for /F "tokens=*" %%A in (%store%) do (
 		REM WinGet install will upgrade if installed already
 		echo -- %%A --
-		winget install -e --accept-package-agreements "%%A" 
+		winget install -e --accept-package-agreements "%%A"
 	)
 ) else (
 	echo --- No winget file for %COMPUTERNAME% ---
 )
 
-REM Update pip if python installed
-set piplist=pip/%COMPUTERNAME%.txt
-
-if NOT EXIST %piplist% (
-	REM use default if no machine specific exists
-	set piplist=pip/default.txt
-)
-
-find /C "Python" %store% > NUL
-if %ERRORLEVEL% EQU 0 (
-	echo --- Python: Update PIP ---
-	python -m ensurepip --upgrade
-
-	REM Install python modules if list file exists
-	if EXIST %piplist% (
-		echo --- Python: Installing modules ---
-		python -m pip install -r %piplist%
-	) else (
-		echo --- No Pip file for %COMPUTERNAME% ---
-	)
-)
 
 echo --- Done! ---
